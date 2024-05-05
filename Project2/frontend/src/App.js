74ab1de4ddeb27bb42459d036acc93a26e504734
MohammedZohaib-hub <133938171+MohammedZohaib-hub@users.noreply.github.com>
2024-05-05 14:02:27 +0530

    Add files via upload

---
+++ Project2/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UrlForm from "./UrlForm";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="mt-4 text-center">URL Shortner</h1>
+      <UrlForm />
+    </div>
+  );
+}
+
+export default App;
